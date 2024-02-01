2453140992c3855c4e9e72f58f3d127c84aa252a
sordinicaro <sordinicaro1@gmail.com>
2024-02-01 14:48:49 -0300

    mètodo busqueda por ID terminado

---
+++ src/interfaces/rickAndMorty.ts
@@ -0,0 +1,26 @@
+interface rickAndMorty{
+
+    results:[
+    id: number,
+    name: string,
+    status: string,
+    species: string,
+    type: string,
+    gender: string,
+    origin: {
+        name: string,
+        url: string
+    },
+    location: {
+        name: string,
+        url: string
+    },
+    image: string,
+    episode: string[],
+    url: string,
+    created: string
+   
+    ]
+}
+
+export { rickAndMorty }
--- src/model/model.ts
+++ src/model/model.ts
@@ -1,32 +1,87 @@
 import dotenv from "dotenv"
+import { rickAndMorty } from "../interfaces/rickAndMorty"
 
 dotenv.config()
 
-const apiRAM : any = process.env.API_KEY
-const character : any = process.env.CHARACTER
+const apiRAM: any = process.env.API_KEY
+const character: any = process.env.CHARACTER
 
 
 
 
-const getAllData = async () => {
-    try{
-       const response = await fetch(apiRAM + character);
-       
-       if (!response.ok) {
-        throw new Error("error to fetch");
+const getAllData = async (): Promise<rickAndMorty | undefined> => {
+  try {
+    const response = await fetch(apiRAM + character);
 
-    }
-       
-       const rickandmortyData = await response.json();
-       const resultsRAM = rickandmortyData.results;
-       console.log(resultsRAM);
-    } catch (error) {
-        console.log(error);
+    if (!response.ok) {
+      throw new Error("error to fetch");
 
     }
-    
-    
-    
+
+    const rickandmortyData = await response.json();
+    const resultsRAM = rickandmortyData.results;
+    return resultsRAM;
+  } catch (error) {
+    console.log(error);
+
+  }
+
+
+
 }
 
 getAllData();
+
+
+
+
+// const getAllTitlesCharcters = async () => {
+//   try {
+
+//     const characters: any = await getAllData();
+//     if (characters instanceof Error) {
+//       throw characters;
+//     }
+
+//     const characterNames = characters.filter((character: any) => character.results.name);
+
+//     return characterNames;
+//   } catch (error) {
+//     console.log(error);
+
+//   }
+// }
+// getAllTitlesCharcters()
+
+
+const getCharacterById = async (id: any) => {
+  try {
+
+    const characters: any = await getAllData();
+    if (characters instanceof Error) {
+      throw characters;
+    }
+    const characterById = characters.find((character: any) => id === character.id)
+
+
+    return characterById;
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+getCharacterById(18)
+
+const main = async () => {
+  const allData = await getAllData();
+  console.log(allData);
+  // const allCharacters = await getAllTitlesCharcters();
+  // console.log(allCharacters);
+  const byId= await getCharacterById(18)
+  console.log(byId);
+
+}
+
+main();
+
